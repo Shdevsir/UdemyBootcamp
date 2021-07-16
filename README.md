29aca742220504c7cda4413392a1ba848d910d13
Shdevsir <shdevsir@gmail.com>
2021-07-16 11:44:03 +0300

    Add project_1 and add project_2

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# UdemyBootcamp
+# In this repository course result UdemyBootcamp 2021
+
+* project_1 - dice rolling
+* project_2 - dictionary
+* project_3 - hangman game
---
+++ project_1/main.py
@@ -0,0 +1,42 @@
+import random
+number = random.randint(1, 6)
+x = "yes"
+while x == "yes":
+    number = random.randint(1, 6)
+    if(number == 1):
+        print("-------")
+        print("|     |")
+        print("|  0  |")
+        print("|     |")
+        print("-------")
+    elif(number == 2):
+        print("-------")
+        print("|    0|")
+        print("|     |")
+        print("|0    |")
+        print("-------")
+    elif(number == 3):
+        print("-------")
+        print("|    0|")
+        print("|  0  |")
+        print("|0    |")
+        print("-------")
+    elif(number == 4):
+        print("-------")
+        print("|0   0|")
+        print("|     |")
+        print("|0   0|")
+        print("-------")
+    elif(number == 5):
+        print("-------")
+        print("|0   0|")
+        print("|  0  |")
+        print("|0   0|")
+        print("-------")
+    elif(number == 6):
+        print("-------")
+        print("|0   0|")
+        print("|0   0|")
+        print("|0   0|")
+        print("-------")
+    x = input("Try again?(yes or not): ")
